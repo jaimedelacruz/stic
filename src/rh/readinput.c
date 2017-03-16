56d3661e3c4470cdec6e3851296e6497254d4b55
Jaime de la Cruz Rodriguez <jaime@astro.su.se>
2017-03-16 11:14:01 +0100

    changed default values for S_INTERPOLATION

--- src/rh/readinput.c
+++ src/rh/readinput.c
@@ -167,9 +167,9 @@ void readInput()
     {"XDR_ENDIAN", "TRUE", FALSE, KEYWORD_OPTIONAL,
      &input.xdr_endian, setboolValue},
 
-    {"S_INTERPOLATION", "PARABOLIC", FALSE, KEYWORD_DEFAULT,
+    {"S_INTERPOLATION", "BEZIER3", FALSE, KEYWORD_DEFAULT,
      &input.S_interpolation, set_S_Interpolation},
-    {"S_INTERPOLATION_STOKES", "DELOLIN", FALSE, KEYWORD_DEFAULT,
+    {"S_INTERPOLATION_STOKES", "DELO_BEZIER3", FALSE, KEYWORD_DEFAULT,
      &input.S_interpolation_stokes, set_S_Interpolation_stokes},
     
     {"INTERPOLATE_3D", "BICUBIC_3D", FALSE, KEYWORD_DEFAULT,
