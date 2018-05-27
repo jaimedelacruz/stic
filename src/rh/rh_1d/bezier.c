a87c01916394348244e14b1f6f4d1409829cc6b4
Jaime de la Cruz Rodriguez <jaime@astro.su.se>
2018-05-27 19:50:01 +0200

    uncommited changes

--- src/rh/rh_1d/bezier.c
+++ src/rh/rh_1d/bezier.c
@@ -23,7 +23,6 @@
 
 #include <math.h>
 #include <string.h>    // memcpy, memset
-#include <x86intrin.h> // Intrinsic SSE instructions
 //
 #include "rh.h"
 #include "error.h"
@@ -37,6 +36,7 @@
 
 #define min(a,b) (((a)<(b))?(a):(b))
 #define max(a,b) (((a)>(b))?(a):(b))
+#define swap(a,b,c) (c=a,a=b,b=c)
 
 /* --- Global variables --                             -------------- */
 
@@ -54,6 +54,100 @@ static const double ident[4][4] =
    {0.0, 0.0, 1.0, 0.0},
    {0.0, 0.0, 0.0, 1.0}};
 
+
+/* ----------------------------------------------------------------------- */
+
+void solveLinearFast(double A[4][4], double B[4])
+{
+  
+  // --- Simple Gaussian elimination with partial pivoting --- //
+  
+  // A is the matrix with coeffs that multiply X. B is the right hand
+  // term (on input). The result is overwritten into B. All operations
+  // are done in-place
+  // It uses the macro "swap" to swap the values of two rows when a pivot
+  // is found
+
+  // The algorithm is a pretty standard Gauss-Jordan algorithm, here optimized a
+  // bit for performance.
+  
+  register int i,j,k;
+  int maxrow,swapme;
+  double maxel,tmp;
+  
+  
+  for (i=0; i<4; i++) {
+
+    // Find pivot
+
+    maxel = fabs(A[i][i]);
+    maxrow = i, swapme = 0;
+    
+    for (k=i+1; k<4; k++){
+      tmp = fabs(A[k][i]);
+      if(tmp > maxel){
+	maxel = tmp;
+	maxrow = k, swapme = 1;
+      }
+    }
+    
+    // swap
+    if(swapme){
+      for (k=i; k<4;k++) swap(A[maxrow][k],A[i][k],tmp);  
+      swap(B[maxrow],B[i],tmp); 
+    }
+
+    // Set to zero relevant columns
+    for (k=i+1; k<4; k++){
+      tmp = -A[k][i]/A[i][i];
+      A[k][i] = 0.0;
+      for ( j=i+1; j<4; j++) {
+	A[k][j] += tmp * A[i][j];
+      }
+      B[k] += tmp*B[i];
+    }
+  }
+
+  
+  // Solve upper triagonal system and store in B
+  
+  for (i=3; i>=0; i--) {
+    B[i] /= A[i][i];
+    for ( k=i-1;k>=0; k--) {
+      B[k] -= A[k][i] * B[i];
+    }
+  }
+  
+}
+
+/* ----------------------------------------------------------------------- */
+
+inline void m4inv(double MI[4][4]){
+
+  /* ---
+     In-place Shipley-Coleman matrix inversion
+     Fast, but ... how accurate?? Pivoting is always done in the diagonal.
+     Copied here just in case the SIMD matrix inversion
+     gives troubles.
+     --- */
+  
+  register int k, i, j;
+  for( k=0;k<4;k++){
+    MI[k][k]=-1.0/MI[k][k];         // the pivot element
+    for( i=0;i<4;++i) if(i!=k) MI[i][k]*=MI[k][k];//the pivot column
+    for( i=0;i<4;++i)           //elements not in a pivot row or column
+      if(i!=k)
+	for( j=0;j<4;++j)
+	  if(j!=k)
+	    MI[i][j]+=MI[i][k]*MI[k][j];
+    for( i=0;i<4;++i)           //elements in a pivot row
+      if(i!=k)
+	MI[k][i]*=MI[k][k];
+  }
+  for( i=0;i<4;++i)
+    for( j=0;j<4;++j) MI[i][j]=-MI[i][j];
+  return;
+}
 /* --------------------------------------------------------------- */
 
 inline double sign(const double val)
@@ -162,139 +256,6 @@ inline void Svec(int k, double **S, double *Sf)
 
 /* -------------------------------------------------------------------------- */
 
-void SIMD_MatInv(float* src)
-{
-  /* --- 
-
-     Very fast in-place 4x4 Matrix inversion using SIMD instrutions
-     Only works with 32-bits floats. It uses Cramer's rule.
-     
-     Provided by Intel
-
-     Requires SSE instructions but all x86 machines since 
-     Pentium III have them.
-     
-     --- */
-  
-  __m128 minor0, minor1, minor2, minor3;
-  __m128 row0, row1, row2, row3;
-  __m128 det, tmp1;
-  // -----------------------------------------------
-  tmp1 = _mm_loadh_pi(_mm_loadl_pi(tmp1, (__m64*)(src)), (__m64*)(src+ 4));
-  row1 = _mm_loadh_pi(_mm_loadl_pi(row1, (__m64*)(src+8)), (__m64*)(src+12));
-  row0 = _mm_shuffle_ps(tmp1, row1, 0x88);
-  row1 = _mm_shuffle_ps(row1, tmp1, 0xDD);
-  tmp1 = _mm_loadh_pi(_mm_loadl_pi(tmp1, (__m64*)(src+ 2)), (__m64*)(src+ 6));
-  row3 = _mm_loadh_pi(_mm_loadl_pi(row3, (__m64*)(src+10)), (__m64*)(src+14));
-  row2 = _mm_shuffle_ps(tmp1, row3, 0x88);
-  row3 = _mm_shuffle_ps(row3, tmp1, 0xDD);
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(row2, row3);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  minor0 = _mm_mul_ps(row1, tmp1);
-  minor1 = _mm_mul_ps(row0, tmp1);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor0 = _mm_sub_ps(_mm_mul_ps(row1, tmp1), minor0);
-  minor1 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor1);
-  minor1 = _mm_shuffle_ps(minor1, minor1, 0x4E);
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(row1, row2);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  minor0 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor0);
-  minor3 = _mm_mul_ps(row0, tmp1);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor0 = _mm_sub_ps(minor0, _mm_mul_ps(row3, tmp1));
-  minor3 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor3);
-  minor3 = _mm_shuffle_ps(minor3, minor3, 0x4E);
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(_mm_shuffle_ps(row1, row1, 0x4E), row3);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  row2 = _mm_shuffle_ps(row2, row2, 0x4E);
-  minor0 = _mm_add_ps(_mm_mul_ps(row2, tmp1), minor0);
-  minor2 = _mm_mul_ps(row0, tmp1);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor0 = _mm_sub_ps(minor0, _mm_mul_ps(row2, tmp1));
-  minor2 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor2);
-  minor2 = _mm_shuffle_ps(minor2, minor2, 0x4E);
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(row0, row1);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  minor2 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor2);
-  minor3 = _mm_sub_ps(_mm_mul_ps(row2, tmp1), minor3);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor2 = _mm_sub_ps(_mm_mul_ps(row3, tmp1), minor2);
-  minor3 = _mm_sub_ps(minor3, _mm_mul_ps(row2, tmp1));
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(row0, row3);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  minor1 = _mm_sub_ps(minor1, _mm_mul_ps(row2, tmp1));
-  minor2 = _mm_add_ps(_mm_mul_ps(row1, tmp1), minor2);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor1 = _mm_add_ps(_mm_mul_ps(row2, tmp1), minor1);
-  minor2 = _mm_sub_ps(minor2, _mm_mul_ps(row1, tmp1));
-  // -----------------------------------------------
-  tmp1 = _mm_mul_ps(row0, row2);
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
-  minor1 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor1);
-  minor3 = _mm_sub_ps(minor3, _mm_mul_ps(row1, tmp1));
-  tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
-  minor1 = _mm_sub_ps(minor1, _mm_mul_ps(row3, tmp1));
-  minor3 = _mm_add_ps(_mm_mul_ps(row1, tmp1), minor3);
-  // -----------------------------------------------
-  det = _mm_mul_ps(row0, minor0);
-  det = _mm_add_ps(_mm_shuffle_ps(det, det, 0x4E), det);
-  det = _mm_add_ss(_mm_shuffle_ps(det, det, 0xB1), det);
-  tmp1 = _mm_rcp_ss(det);
-  det = _mm_sub_ss(_mm_add_ss(tmp1, tmp1), _mm_mul_ss(det, _mm_mul_ss(tmp1, tmp1)));
-  det = _mm_shuffle_ps(det, det, 0x00);
-  minor0 = _mm_mul_ps(det, minor0);
-  _mm_storel_pi((__m64*)(src), minor0);
-  _mm_storeh_pi((__m64*)(src+2), minor0);
-  minor1 = _mm_mul_ps(det, minor1);
-  _mm_storel_pi((__m64*)(src+4), minor1);
-  _mm_storeh_pi((__m64*)(src+6), minor1);
-  minor2 = _mm_mul_ps(det, minor2);
-  _mm_storel_pi((__m64*)(src+ 8), minor2);
-  _mm_storeh_pi((__m64*)(src+10), minor2);
-  minor3 = _mm_mul_ps(det, minor3);
-  _mm_storel_pi((__m64*)(src+12), minor3);
-  _mm_storeh_pi((__m64*)(src+14), minor3);
-}
-
-/* -------------------------------------------------------------------------- */
-
-inline void Bezier3_coeffs_old(double dt, double *alpha, double *beta,
-		    double *gamma, double *theta, double *eps)
-{
-  /* ---
-
-     Integration coeffs. for cubic Bezier interpolants
-     Use Taylor expansion if dtau is small
-     
-     --- */
-  
-  double dt2 = dt*dt, dt3 = dt2 * dt;//,  dt4;
-  if(dt < 0.05){
-    *eps =    1.0 - dt + 0.50 * dt2 - dt3 / 6.0;
-    *alpha = 0.25 * dt - 0.05 * dt2 + dt3 / 120.0;// - dt4 / 840.0;
-    *beta  = 0.25 * dt - 0.20 * dt2 + dt3 / 12.0;//  - dt4 / 42.0;
-    *gamma = 0.25 * dt - 0.10 * dt2 + dt3 * 0.025;// - dt4 / 210.0;
-    *theta = 0.25 * dt - 0.15 * dt2 + dt3 * 0.05 ;// - dt4 / 84.0;
-    //return;
-  }else{
-    if(dt >40.) *eps = 0.0;
-    else        *eps = exp(-dt);
-    *alpha = 1.0 + (6.0*dt - 3.0*dt2 + 6.0 * (*eps - 1.0)) / dt3;
-    *beta  = (6.0 - (6.0 + dt * (6.0 + dt * (3.0 + dt))) * (*eps))    / dt3;
-    *gamma = 3.0 * (6.0 + (dt-4.0)*dt -  (6.0 + 2.* dt) * (*eps))  / dt3;
-    *theta = 3.0 * (*eps * (6.0 + dt2 + 4.0 * dt) + 2.0 * dt - 6.0) / dt3;
-    //return;
-  }
-
-  
-}
-/* -------------------------------------------------------------------------- */
-
 inline void Bezier3_coeffs(double dt, double *alpha, double *beta,
 		    double *gamma, double *theta, double *eps)
 {
@@ -307,22 +268,29 @@ inline void Bezier3_coeffs(double dt, double *alpha, double *beta,
   
   double dt2 = dt*dt, dt3 = dt2 * dt;//,  dt4;
   if(dt < 0.05){
-    *eps =   1.0 - dt + 0.50 * dt2 - dt3 *  0.16666666666666666666;
-    *alpha = 0.25 * dt - 0.20 * dt2 + dt3 * 0.08333333333333333333;// - dt4 / 840.0;
-    *beta  = 0.25 * dt - 0.05 * dt2 + dt3 * 0.00833333333333333333;//  - dt4 / 42.0;
+    *eps =   1.0 - dt + 0.50 * dt2 - dt3 *  0.166666666666666666666666667;
+    *alpha = 0.25 * dt - 0.20 * dt2 + dt3 * 0.083333333333333333333333333;// - dt4 / 840.0;
+    *beta  = 0.25 * dt - 0.05 * dt2 + dt3 * 0.008333333333333333333333333;//  - dt4 / 42.0;
     *gamma = 0.25 * dt - 0.15 * dt2 + dt3 * 0.050;// - dt4 / 210.0;
     *theta = 0.25 * dt - 0.10 * dt2 + dt3 * 0.025;// - dt4 / 84.0;
     return;
   }else{
-    *eps = ((dt>20.)? 0.0 : exp(-dt));
-    *alpha = -(-6.0+(6.0+6.0*dt+3.0*dt2+dt3)*(*eps))/dt3;
-    *beta  = (-6.0 + dt*(6.0+dt*(dt-3.0)) +6.0*(*eps)) / dt3;
-    *gamma = 3.0 * (2.0*dt-6.0 + *eps*(6.0+dt*(dt+4.0))) / dt3;
-    *theta = 3.0 * (-2.0*(*eps)*(dt+3.0) +6.0+(dt-4.0)*dt) / dt3;
-    return;
+    if((dt>30.)){
+      *eps = 0.0;
+      *alpha = 6.0 / dt3;
+      *beta = 1.0 - (*alpha)*(1.0-dt) - 3.0/dt;
+      *gamma = *alpha*(dt-3.0);
+      *theta = 3.0*(*alpha + (dt-4.0)/dt2);
+      return;
+    }else{
+      *eps = exp(-dt);
+      *alpha = -(-6.0+(6.0+6.0*dt+3.0*dt2+dt3)*(*eps))/dt3;
+      *beta  = (-6.0 + dt*(6.0+dt*(dt-3.0)) +6.0*(*eps)) / dt3;
+      *gamma = 3.0 * (2.0*dt-6.0 + *eps*(6.0+dt*(dt+4.0))) / dt3;
+      *theta = 3.0 * (-2.0*(*eps)*(dt+3.0) +6.0+(dt-4.0)*dt) / dt3;
+      return;
+    }
   }
-
-  
 }
 void PiecewiseStokesBezier3(int nspect, int mu, bool_t to_obs,
 			    double *chi, double **S, double **I, double *Psi)
@@ -339,6 +307,8 @@ void PiecewiseStokesBezier3(int nspect, int mu, bool_t to_obs,
      --- */
   
   static const char routineName[] = "PiecewiseStokesBezier3";
+  static const int siz_mat = 16*sizeof(double), siz_vec = 4*sizeof(double);
+
   register int k, n, m, i, j;
   
   int    Ndep = geometry.Ndep, k_start, k_end, dk, k_last, ref_index;
@@ -348,7 +318,7 @@ void PiecewiseStokesBezier3(int nspect, int mu, bool_t to_obs,
   double dsup,dsdn,dt,eps=0,alpha=0,beta=0,gamma=0,theta=0;
   double Ku[4][4], K0[4][4], Kd[4][4], dKu[4][4], dK0[4][4];
   double Su[4], S0[4], Sd[4], dSu[4], dS0[4];
-  double A[4][4], Ma[4][4], Mb[4][4], Mc[4][4], V0[4], V1[4];
+  double A[4][4], Ma[4][4], Mb[4][4], Mc[4][4], V0[4];//, V1[4];
   double imu = 1.0 / geometry.muz[mu];
   double Md[4][4];
   //double *z = geometry.tau_ref;
@@ -507,12 +477,13 @@ void PiecewiseStokesBezier3(int nspect, int mu, bool_t to_obs,
       }
     }
     
-    dt = dtau_uw, dt03 = dt / 3.0;
+    dt = dtau_uw, dt03 = dt * 0.3333333333333333333333333333333;
 
     
     /* --- Bezier3 coeffs. ---- */
       
     Bezier3_coeffs(dt, &beta, &alpha, &theta, &gamma, &eps);
+
     
     /* --- Diagonal operator --- */
       
@@ -557,24 +528,22 @@ void PiecewiseStokesBezier3(int nspect, int mu, bool_t to_obs,
       
       
     /* --- Solve linear system to get the intensity --- */
-      
-    //SIMD_MatInv(Md[0]); // Invert Md
-    m4inv(Md); // Invert Md
-    m4v(Md,V0,V1);      // Multiply Md^-1 * V0
+     
+    solveLinearFast(Md,V0);
+
     
-    
-    for(i=0;i<4;i++) I[i][k] = V1[i];
+    for(i=0;i<4;i++) I[i][k] = V0[i];
 
     
     /* --- Shift values for next depth --- */
       
-    memcpy(Su,   S0, 4*sizeof(double));
-    memcpy(S0,   Sd, 4*sizeof(double));
-    memcpy(dSu, dS0, 4*sizeof(double));
+    memcpy(Su,   S0, siz_vec);
+    memcpy(S0,   Sd, siz_vec);
+    memcpy(dSu, dS0, siz_vec);
       
-    memcpy(Ku[0],   K0[0], 16*sizeof(double));
-    memcpy(K0[0],   Kd[0], 16*sizeof(double));
-    memcpy(dKu[0], dK0[0], 16*sizeof(double));
+    memcpy(Ku[0],   K0[0], siz_mat);
+    memcpy(K0[0],   Kd[0], siz_mat);
+    memcpy(dKu[0], dK0[0], siz_mat);
       
     dtau_uw = dtau_dw;
     dsup    = dsdn;
@@ -789,34 +758,4 @@ void Piecewise_Bezier3(int nspect, int mu, bool_t to_obs,
 
 
 
-/* -------------------------------------------------------------------------- */
 
-
-void m4inv(double MI[4][4]){
-
-  /* --- 
-     In-place Shipley-Coleman matrix inversion
-     Fast, but ... how accurate?? Pivoting is always done in the diagonal.
-     Copied here just in case the SIMD matrix inversion
-     gives troubles.
-     --- */
-  
-  register int k, i, j;
-  for( k=0;k<4;k++){
-    MI[k][k]=-1.0/MI[k][k];         // the pivot element
-    for( i=0;i<4;++i) if(i!=k) MI[i][k]*=MI[k][k];//the pivot column
-    for( i=0;i<4;++i)           //elements not in a pivot row or column
-      if(i!=k)
-	for( j=0;j<4;++j)
-	  if(j!=k)
-	    MI[i][j]+=MI[i][k]*MI[k][j];
-    for( i=0;i<4;++i)           //elements in a pivot row
-      if(i!=k)
-	MI[k][i]*=MI[k][k];
-  }
-  for( i=0;i<4;++i)
-    for( j=0;j<4;++j) MI[i][j]=-MI[i][j];
-  return;
-}
-
-/* -------------------------------------------------------------------------- */
